92cfd490edae85a1865e5b599a830f63dc6f3b7a
Cristobal2929 <dayron100813@gmail.com>
2025-08-24 04:56:56 +0200

    Curso completo generado y listo para vender

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 const BACKEND_URL = "https://english-course-pwa-backend.onrender.com";
 
 async function cargarLeccion(num){
-    const res = await fetch(`lecciones/leccion${num}.json`);
+const BACKEND_URL = "https://english-course-pwa-backend.onrender.com";
     const data = await res.json();
     document.getElementById("app").innerText = data.contenido;
 }
 
 async function cargarTest(num){
-    const res = await fetch(`tests/test${num}.json`);
+const BACKEND_URL = "https://english-course-pwa-backend.onrender.com";
     const data = await res.json();
     console.log(data);
 }
